df7a3b8df06a52bb952003befc021ad12735783f
Ostapcov Danil <dost4657@gmail.com>
2024-02-11 01:04:49 +0300

    added userId to the event.context and fixed infinite redirect

--- server/middleware/auth.ts
+++ server/middleware/auth.ts
@@ -1,10 +1,14 @@
 import { SESSION_STORAGE } from "~/utils/sessionStorage"
 
-export default defineEventHandler( async (event) => {
-    if (!event.path.includes('login/'))
+export default defineEventHandler((event) => {
+    if (event.path.includes('login'))
         return
 
     const sessionId = getCookie(event, 'session_id')
-    if (!sessionId || !SESSION_STORAGE.get(sessionId))
-        await sendRedirect(event, 'login/', 301);
+    if (!sessionId)
+        return sendRedirect(event, '/login');
+    const userId = SESSION_STORAGE.get(sessionId)
+    if (!userId)
+        return sendRedirect(event, '/login');
+    event.context.userId = userId
   })
