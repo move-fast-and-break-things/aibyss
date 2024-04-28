3586debf771173c7c8541c6b8b7ee40ee0337611
Ostapcov Danil <dost4657@gmail.com>
2024-04-28 12:58:45 +0300

    remove unnecessary migration

--- migrations/20240210182114_init/migration.sql
+++ migrations/20240210182114_init/migration.sql
@@ -2,8 +2,8 @@
 CREATE TABLE "User" (
     "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
     "password" TEXT NOT NULL,
-    "name" TEXT NOT NULL
+    "username" TEXT NOT NULL
 );
 
 -- CreateIndex
-CREATE UNIQUE INDEX "User_name_key" ON "User"("name");
+CREATE UNIQUE INDEX "User_name_key" ON "User"("username");
