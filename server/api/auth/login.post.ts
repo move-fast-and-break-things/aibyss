a753279650599e9b7134b92315470ef8658414c2
Ostapcov Danil <dost4657@gmail.com>
2024-04-28 12:45:02 +0300

    1234

--- server/api/auth/login.post.ts
+++ server/api/auth/login.post.ts
@@ -12,29 +12,28 @@ const prisma = new PrismaClient()
 
 
 export default defineEventHandler(async (event) => {
-    const body = await readBody(event);
-    const result = await readValidatedBody(event, body => userSchema.safeParse(body));
+  const body = await readBody(event);
+  const result = await readValidatedBody(event, body => userSchema.safeParse(body));
 
-    if (!result.success)
-      throw createError({statusCode: 422, statusMessage: 'Username and password field is required'});
+  if (!result.success)
+    throw createError({ statusCode: 422, statusMessage: 'Username and password field is required' });
+  const user = await prisma.user.findFirst({ where: { username: result.data.username } })
+  if (!user)
+    throw createError({ statusCode: 401, statusMessage: `Unauthorized` });
 
-    const user = await prisma.user.findFirst({where: {name: result.data.username}})
-    if (!user)
-      throw createError({statusCode: 404, statusMessage: `User with username=${result.data.username} not found`});
-
-    const hash = user.password;
-    const match = await bcrypt.compare(result.data.password, hash);
-    if (match) {
-      const sessionId = crypto.randomUUID();
-      setCookie(event, 'session_id', sessionId, {
-        httpOnly: true,
-        secure: true,
-        maxAge: SESSION_TTL,
-      });
-      SESSION_STORAGE.set(sessionId, user.id)
-      return { statusCode: 200 }
-    } 
-    else {
-      throw createError({statusCode: 401, statusMessage: 'Invalid password or username'});
-    }
-  })
+  const hash = user.password;
+  const match = await bcrypt.compare(result.data.password, hash);
+  if (match) {
+    const sessionId = crypto.randomUUID();
+    setCookie(event, 'session_id', sessionId, {
+      httpOnly: true,
+      secure: true,
+      maxAge: SESSION_TTL,
+    });
+    SESSION_STORAGE.set(sessionId, user.id)
+    return sendRedirect(event, '/')
+  }
+  else {
+    throw createError({ statusCode: 401, statusMessage: 'Invalid password or username' });
+  }
+})
