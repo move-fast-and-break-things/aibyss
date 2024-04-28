e28082469cc815a79cbbb1e78dec174b0ee1d8d0
Yurij Mikhalevich <yurij@mikhalevi.ch>
2024-04-28 21:12:12 +0400

    skip generatic prisma client in CI

--- tests/e2e/global-setup.ts
+++ tests/e2e/global-setup.ts
@@ -3,7 +3,7 @@ import { seedTestUsers } from "../utils/seed-test-users";
 
 async function globalSetup() {
   console.log("initializing the database...");
-  child_process.execSync("npx prisma migrate reset --force", { stdio: "inherit" });
+  child_process.execSync("npx prisma migrate reset --force --skip-generate", { stdio: "inherit" });
 
   console.log("seeding the test users...");
   seedTestUsers();
