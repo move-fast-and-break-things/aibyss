c461d2afe2588f085d02116c9441a8cdb6b74c94
Yurij Mikhalevich <yurij@mikhalevi.ch>
2024-07-29 11:02:08 +0400

    fix tests

--- utils/world.spec.ts
+++ utils/world.spec.ts
@@ -6,32 +6,30 @@ it("correctly removes the smaller bot when the bigger bot eats it", () => {
   world.addBot("1");
   world.addBot("2");
 
-  // @ts-expect-error - world.bots is private
-  const bot1 = world.bots.get("1") as BotSprite;
+  // @ts-expect-error - world.botSpawns is private
+  const bot1 = [...world.botSpawns.values()].find(bot => bot.botId === "1") as BotSprite;
   bot1.radius = 10;
   bot1.x = 10;
   bot1.y = 10;
 
-  // @ts-expect-error - world.bots is private
-  const bot2 = world.bots.get("2") as BotSprite;
+  // @ts-expect-error - world.botSpawns is private
+  const bot2 = [...world.botSpawns.values()].find(bot => bot.botId === "2") as BotSprite;
   bot2.radius = 5;
   bot2.x = 10;
   bot2.y = 10;
 
   world.checkCollisions("1");
 
-  // @ts-expect-error - world.bots is private
-  expect(world.bots.has("1")).toBe(true);
-  // @ts-expect-error - world.bots is private
-  expect(world.bots.has("2")).toBe(false);
+  expect(world.hasBot("1")).toBe(true);
+  expect(world.hasBot("2")).toBe(false);
 });
 
 it("correctly removes food when the bot eats it", () => {
   const world = new World({ width: 100, height: 100 });
   world.addBot("1");
 
-  // @ts-expect-error - world.bots is private
-  const bot = world.bots.get("1") as BotSprite;
+  // @ts-expect-error - world.botSpawns is private
+  const bot = [...world.botSpawns.values()].find(bot => bot.botId === "1") as BotSprite;
   bot.x = 10;
   bot.y = 10;
 
@@ -53,8 +51,8 @@ it("shouldn't crash when bot eats two food items and one of them with the last i
   const world = new World({ width: 100, height: 100 });
   world.addBot("1");
 
-  // @ts-expect-error - world.bots is private
-  const bot = world.bots.get("1") as BotSprite;
+  // @ts-expect-error - world.botSpawns is private
+  const bot = [...world.botSpawns.values()].find(bot => bot.botId === "1") as BotSprite;
   bot.x = 10;
   bot.y = 10;
 
