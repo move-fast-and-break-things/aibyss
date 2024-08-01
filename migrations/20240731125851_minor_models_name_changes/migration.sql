ef41a8a8f07464ee2e11e5e3ceb5c6a83d30bae9
Andrey <145541424+PlutoniumSoup@users.noreply.github.com>
2024-08-01 10:28:54 +0300

    Update migrations/20240731125851_minor_models_name_changes/migration.sql
    Co-authored-by: Yurij Mikhalevich <yurij@mikhalevi.ch>

--- migrations/20240731125851_minor_models_name_changes/migration.sql
+++ migrations/20240731125851_minor_models_name_changes/migration.sql
@@ -1,20 +1,3 @@
-/*
-  Warnings:
-
-  - You are about to drop the `Game_stats` table. If the table is not empty, all the data it contains will be lost.
-  - You are about to drop the `Games` table. If the table is not empty, all the data it contains will be lost.
-
-*/
--- DropTable
-PRAGMA foreign_keys=off;
-DROP TABLE "Game_stats";
-PRAGMA foreign_keys=on;
-
--- DropTable
-PRAGMA foreign_keys=off;
-DROP TABLE "Games";
-PRAGMA foreign_keys=on;
-
 -- CreateTable
 CREATE TABLE "Game" (
     "game_id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
