0d1d28e78484701ac2c8918075b8f9b7c3cd5e94
Dhanush <dhanushxenocrate@gmail.com>
2024-10-05 14:22:36 +0000

    Resolved requested changes

--- server/api/auth/login.post.ts
+++ server/api/auth/login.post.ts
@@ -3,18 +3,7 @@ import bcrypt from "bcrypt";
 import { z } from "zod";
 import { SESSION_COOKIE_NAME, SESSION_STORAGE, SESSION_TTL_SECONDS } from "~/utils/sessionStorage";
 import prisma from "~/utils/db";
-
-// Constants
-const STATUS_CODES = {
-  UNPROCESSABLE_ENTITY: 422,
-  UNAUTHORIZED: 401,
-};
-
-const ERROR_MESSAGES = {
-  VALIDATION_ERROR: "Username and password field is required",
-  USER_NOT_FOUND: "Invalid username or password",
-  INVALID_CREDENTIALS: "Invalid username or password",
-};
+import { STATUS_CODES, ERROR_MESSAGES } from '~/utils/stat/statusCodes';
 
 const userSchema = z.object({
   username: z.string(),
@@ -22,23 +11,18 @@ const userSchema = z.object({
 });
 
 export default defineEventHandler(async (event) => {
-  // Validate request body
   const result = await readValidatedBody(event, body => userSchema.safeParse(body));
   if (!result.success) {
     throw createError({ statusCode: STATUS_CODES.UNPROCESSABLE_ENTITY, statusMessage: ERROR_MESSAGES.VALIDATION_ERROR });
   }
 
-  // Check for user existence
   const user = await prisma.user.findFirst({ where: { username: result.data.username } });
   if (!user) {
-    // User not found, send a generic message to prevent enumeration attacks
     throw createError({ statusCode: STATUS_CODES.UNAUTHORIZED, statusMessage: ERROR_MESSAGES.USER_NOT_FOUND });
   }
 
-  // Compare password
   const isPasswordValid = await bcrypt.compare(result.data.password, user.password);
   if (isPasswordValid) {
-    // Successful login, create a session
     const sessionId = crypto.randomUUID();
     setCookie(event, SESSION_COOKIE_NAME, sessionId, {
       httpOnly: true,
@@ -48,7 +32,6 @@ export default defineEventHandler(async (event) => {
     SESSION_STORAGE.set(sessionId, user.id);
     return sendRedirect(event, "/");
   } else {
-    // Invalid password
     throw createError({ statusCode: STATUS_CODES.UNAUTHORIZED, statusMessage: ERROR_MESSAGES.INVALID_CREDENTIALS });
   }
 });
