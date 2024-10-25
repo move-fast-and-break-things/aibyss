ebdf608309da9083df0a114c5b612a58616e1b1f
PlutoniumSoap <alephnight.g@gmail.com>
2024-10-25 21:09:36 +0300

    There's a ratelimit for 'Submit' button

--- server/api/bot/submit.post.ts
+++ server/api/bot/submit.post.ts
@@ -5,7 +5,16 @@ const submitBotCodeSchema = z.object({
   code: z.string(),
 });
 
+const rateLimit = new Map<string, number>();
+const RATE_LIMIT_THRESHOLD_IN_MILLIS = 4000;
+
 export default defineEventHandler(async (event) => {
+  const lastRequestTime = rateLimit.get(event.context.user.id);
+
+  if (lastRequestTime && Date.now() - lastRequestTime <= RATE_LIMIT_THRESHOLD_IN_MILLIS) {
+    return { statusCode: 418, statusMessage: "Teapot needs 4 seconds to think about your code" };
+  }
+
   const result = await readValidatedBody(event, body => submitBotCodeSchema.safeParse(body));
   if (!result.success) {
     throw createError({ statusCode: 422, statusMessage: "Username and password field is required" });
@@ -16,6 +25,8 @@ export default defineEventHandler(async (event) => {
     userId: event.context.user.id,
     code: result.data.code,
   });
+
+  rateLimit.set(event.context.user.id, Date.now());
 
   return { statusCode: 200 };
 });
