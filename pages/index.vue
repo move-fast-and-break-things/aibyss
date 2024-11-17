80b89d9ffe002bd2a51601b6b9a3dec3ad4fb41c
aldrin312 <aldrin_fernandez@yahoo.com>
2024-11-17 04:10:27 -0500

    Convert ref's to access DOM instead of .querySelector.

--- pages/index.vue
+++ pages/index.vue
@@ -5,11 +5,20 @@
       class="flex flex-grow p-4"
       style="height: 90%;"
     >
-      <div class="flex h-full min-w-[300px] flex-grow w-1/2 mr-2">
+      <div
+        ref="resizeEditorElement"
+        class="flex min-w-[300px] flex-grow resize-x mr-2"
+      >
         <CodeEditor />
       </div>
-      <div class="w-1 bg-gray-300 cursor-ew-resize" /> <!-- Seperator bar -->
-      <div class="flex h-full min-w-[300px] w-1/2 overflow-auto">
+      <div
+        ref="separator"
+        class="separator"
+      />
+      <div
+        ref="resizeGameElement"
+        class="flex min-w-[300px] resize-x overflow-auto"
+      >
         <GameScreen />
       </div>
     </div>
@@ -25,25 +34,26 @@ export default defineComponent({
     const initialX = ref(0);
     const initialWidthEditor = ref(0);
     const initialWidthGame = ref(0);
+    const resizeEditorElement = ref<HTMLDivElement | null>(null);
+    const resizeGameElement = ref<HTMLDivElement | null>(null);
+    const separator = ref<HTMLDivElement | null>(null);
 
     const startResize = (e: MouseEvent) => {
-      isResizing.value = true;
-      initialX.value = e.clientX;
-      const resizeEditorElement = document.querySelector(".resize-x:first-of-type") as HTMLElement;
-      const resizeGameElement = document.querySelector(".resize-x:last-of-type") as HTMLElement;
-      initialWidthEditor.value = resizeEditorElement.offsetWidth;
-      initialWidthGame.value = resizeGameElement.offsetWidth;
+      if (resizeEditorElement.value && resizeGameElement.value) {
+        isResizing.value = true;
+        initialX.value = e.clientX;
+        initialWidthEditor.value = resizeEditorElement.value.offsetWidth;
+        initialWidthGame.value = resizeGameElement.value.offsetWidth;
+      }
     };
 
     const handleResize = (e: MouseEvent) => {
-      if (isResizing.value) {
+      if (isResizing.value && resizeEditorElement.value && resizeGameElement.value) {
         const dx = e.clientX - initialX.value;
-        const resizeEditorElement = document.querySelector(".resize-x:first-of-type") as HTMLElement;
-        const resizeGameElement = document.querySelector(".resize-x:last-of-type") as HTMLElement;
         const newWidthEditor = initialWidthEditor.value + dx;
         const newWidthGame = initialWidthGame.value - dx;
-        resizeEditorElement.style.width = `${newWidthEditor}px`;
-        resizeGameElement.style.width = `${newWidthGame}px`;
+        resizeEditorElement.value.style.width = `${newWidthEditor}px`;
+        resizeGameElement.value.style.width = `${newWidthGame}px`;
       }
     };
 
@@ -52,24 +62,39 @@ export default defineComponent({
     };
 
     onMounted(() => {
-      const separator = document.querySelector(".separator") as HTMLElement;
-      separator.addEventListener("mousedown", startResize);
+      if (separator.value) {
+        separator.value.addEventListener("mousedown", startResize);
+      }
       document.addEventListener("mousemove", handleResize);
       document.addEventListener("mouseup", stopResize);
     });
 
     onUnmounted(() => {
-      const separator = document.querySelector(".separator") as HTMLElement;
-      separator.removeEventListener("mousedown", startResize);
+      if (separator.value) {
+        separator.value.removeEventListener("mousedown", startResize);
+      }
       document.removeEventListener("mousemove", handleResize);
       document.removeEventListener("mouseup", stopResize);
     });
-
     return {
-      startResize,
-      handleResize,
-      stopResize,
+      resizeEditorElement,
+      resizeGameElement,
+      separator,
     };
   },
 });
 </script>
+
+<style scoped>
+ .separator {
+    @apply w-1 bg-gray-300 cursor-ew-resize;
+  }
+  .resize-x:first-of-type {
+    @apply w-1/2;
+  }
+  .resize-x:last-of-type {
+    @apply w-1/2;
+  } .flex-grow {
+    @apply flex h-full;
+  }
+</style>
