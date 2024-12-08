d5bb60ba42eb4224016cce3aa165d95e6ff79a6a
Yurij Mikhalevich <yurij@mikhalevi.ch>
2024-12-08 15:01:30 +0400

    test(GlobalHeader): quickfix the test, follow up to #90

--- components/GlobalHeader.nuxt.spec.ts
+++ components/GlobalHeader.nuxt.spec.ts
@@ -6,5 +6,5 @@ import GlobalHeader from "./GlobalHeader.vue";
 
 it("mounts the GlobalHeader component", async () => {
   const component = await mountSuspended(GlobalHeader);
-  expect(component.text()).toEqual("Aibyss ?");
+  expect(component.text()).toEqual("Aibyss rating • ?");
 });
