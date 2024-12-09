c1eda8dd8edb8d84bc1a08d093772e4e8462cd8f
Yurij Mikhalevich <yurij@mikhalevi.ch>
2024-12-09 09:47:37 +0400

    chore: update deps

--- other/CodeRunner.spec.ts
+++ other/CodeRunner.spec.ts
@@ -22,21 +22,21 @@ describe("CodeRunner", () => {
 
   it("crashes if the code throws an exception", async () => {
     const codeRunner = new CodeRunner();
-    expect(() => codeRunner.runCode(`(() => { throw Error("I am a test exception") })()`))
+    await expect(() => codeRunner.runCode(`(() => { throw Error("I am a test exception") })()`))
       .rejects
       .toThrow("I am a test exception");
   });
 
   it("can't access the `window` object", async () => {
     const codeRunner = new CodeRunner();
-    expect(() => codeRunner.runCode("JSON.stringify(window.location)"))
+    await expect(() => codeRunner.runCode("JSON.stringify(window.location)"))
       .rejects
       .toThrow("window is not defined");
   });
 
   it("crashes if the code consumes too much memory", async () => {
     const codeRunner = new CodeRunner();
-    expect(() => {
+    await expect(() => {
       return codeRunner.runCode(`
         const array = new Array(70 * 1024 * 1024 / 4);
         array.fill(0);
@@ -46,7 +46,7 @@ describe("CodeRunner", () => {
 
   it("crashes if the code runs for too long", async () => {
     const codeRunner = new CodeRunner();
-    expect(() => codeRunner.runCode("while (true) {}"))
+    await expect(() => codeRunner.runCode("while (true) {}"))
       .rejects
       .toThrow("Script execution timed out");
   });
@@ -55,14 +55,14 @@ describe("CodeRunner", () => {
     const codeRunner = new CodeRunner();
     const result = await codeRunner.runCode("const a = 39; a");
     expect(result).toEqual(39);
-    expect(() => codeRunner.runCode("a"))
+    await expect(() => codeRunner.runCode("a"))
       .rejects
       .toThrow("a is not defined");
   });
 
   it("can't call `fetch`", async () => {
     const codeRunner = new CodeRunner();
-    expect(() => codeRunner.runCode("fetch('https://example.com')"))
+    await expect(() => codeRunner.runCode("fetch('https://example.com')"))
       .rejects
       .toThrow("fetch is not defined");
   });
