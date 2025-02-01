95637829c24b161b4b1c20002009d197e0ae122c
Ivan Pchelka <ivan.pchelka@bk.ru>
2025-02-01 14:14:32 +0300

    Update GameScreen.vue

--- components/GameScreen.vue
+++ components/GameScreen.vue
@@ -280,7 +280,7 @@ watch(gameState, async (newState, prevState) => {
       graphics.circle(food.x, food.y, food.radius);
       graphics.fill("#FF0000");
       app.stage.addChild(graphics);
-      foodRef.value = await Promise.all([Assets.load("/sprites/FishVer1Short.png")]);
+      foodRef.value = await Promise.all([Assets.load("/sprites/FoodVer22.png")]);
     }
 
     // render bots
@@ -323,7 +323,7 @@ watch(gameState, async (newState, prevState) => {
         graphics.circle(food.x, food.y, food.radius);
         graphics.fill("#FF0000");
         appRef.value.stage.addChild(graphics);
-        foodRef.value = await Promise.all([Assets.load("/sprites/FishVer1Short.png")]);
+        foodRef.value = await Promise.all([Assets.load("/sprites/FoodVer22.png")]);
       }
     }
 
